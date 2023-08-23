65fd1ff836209121a1e9a54118ae8670102b0324
Eduardo <eduardopires31@gmail.com>
2023-08-22 22:53:43 -0300

    Adding Google Tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 import '../styles/main.css'
 
@@ -7,6 +8,18 @@ export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Head>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-TNYEKWP2VN"></Script>
+        <Script id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html:`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TNYEKWP2VN');
+  `, }}
+        />
         <link
           rel="alternate"
           type="application/rss+xml"
