e6f2fd7a428215885feef1389f997dd6f64644c1
Eduardo <eduardopires31@gmail.com>
2023-08-22 22:31:43 -0300

    Readding empty post page and updating profile picture

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,7 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Eduard Pirese',
+    title: 'Eduardo Pires',
     site_url: 'https://eduardopires.dev',
     feed_url: 'https://eduardopires.dev/feed.xml'
   })
