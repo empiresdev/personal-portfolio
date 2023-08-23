cd6387527ad2606b0f86ac2ae48109bb660631b6
Eduardo <eduardopires31@gmail.com>
2023-08-22 21:45:24 -0300

    Updating main website

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Eduardo Pires | Mobile Developer',
+    description: 'This is the personal website for Eduardo Pires',
+    image: 'https://flutterando.com.br/flutter-brasil/_next/static/media/EDUARDO-PERFIL.803c15cf.png'
   }
 
   return (
--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Eduard Pirese',
+    site_url: 'https://eduardopires.dev',
+    feed_url: 'https://eduardopires.dev/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
